78781758b6d3892a4cb6391f2a5b41d79a14de22
Art Skvira <art@nimblegecko.com>
2014-11-17 21:03:42 +1100

    Added generic function

--- Dropdown-enums/Controllers/ProfileController.cs
+++ Dropdown-enums/Controllers/ProfileController.cs
@@ -59,6 +59,9 @@ namespace Dropdowns.Controllers
             // [Display] attribute of the Industry enum
             model.IndustryName = GetIndustryName(model.Industry);
 
+            // Or uncomment to use the generic implementation
+            // model.IndustryName = GetEnumDisplayName(model.Industry);
+
             // Display ViewProfile.html page that shows FirstName, Last Name and selected Industry.
             return View(model);
         }
@@ -110,5 +113,25 @@ namespace Dropdowns.Controllers
 
             return displayAttribute[0].Name;
         }
+
+        /// <summary>
+        /// Generic function that obtains value .Neme property of [Display] attribute
+        /// on a supplied enum value. Can be used with any enum, not just 'Industry'
+        /// in this example
+        /// </summary>
+	    private string GetEnumDisplayName<T>(T value) where T: struct
+	    {
+	        // Get the MemberInfo object for supplied enum value
+	        var memberInfo = value.GetType().GetMember(value.ToString());
+	        if (memberInfo.Length != 1)
+		    return null;
+
+	        // Get DisplayAttibute on the supplied enum value
+	        var displayAttribute = memberInfo[0].GetCustomAttributes(typeof(DisplayAttribute), false) as DisplayAttribute[];
+	        if (displayAttribute == null || displayAttribute.Length != 1)
+		    return null;
+
+	        return displayAttribute[0].Name;
+	    }
     }
 }
